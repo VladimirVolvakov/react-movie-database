0e243e1245cf84055102a67b247edebc938e077c
VladimirVolvakov <dr.volvakov@gmail.com>
2022-06-30 23:36:55 +0300

    feat: add Header component markup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import RMDBLogo from '../../assets/react-movie-logo.svg';
+import TMDBLogo from '../../assets/tmdb_logo.svg';
+import { Wrapper, Content, Logo, TheMovieDatabaseLogo } from './Header.styles';
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <Content>
+        <Logo src={RMDBLogo} alt='React movie database logo' />
+        <TheMovieDatabaseLogo src={TMDBLogo} alt='TheMovieDataBase logo' />
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Header;
