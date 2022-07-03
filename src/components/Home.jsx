de9b5e656090e02558661cb568b80c5599138416
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-03 15:44:19 +0300

    feat: add fetch movie data functionality to  Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
 // Hook:
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // Config:
 import { BACKDROP_SIZE, POSTER_SIZE, IMAGE_BASE_URL } from '../config';
+// API:
+import API from '../API';
 // Image:
 import NoImage from '../assets/no_image.jpg';
 
@@ -9,6 +11,29 @@ const Home = () => {
   const [state, setState] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  // Get movies from the DB:
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setIsLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState(prevState => ({
+        ...movies,
+        results: (page > 1) ? [...prevState.results, ...movies.results] : [...movies.results]
+      }));
+    } catch (error) {
+      setError(true);
+    }
+
+    setIsLoading(false);
+  };
+
+  // Initial render of movies:
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
 
   return (
     <div>Home</div>
