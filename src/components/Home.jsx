cdb429137ce74cfbbceed4237873c1386b4b1b23
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-03 19:14:35 +0300

    feat: add HeroImage component and implement it in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,29 @@
 // Hooks:
 import { useEffect, useState } from 'react';
-import { useHomeFetchMovies } from '../hooks/useHomeFetchMovies';     //
+import { useHomeFetchMovies } from '../hooks/useHomeFetchMovies';
 // Config:
 import { BACKDROP_SIZE, POSTER_SIZE, IMAGE_BASE_URL } from '../config';
 // API:
 import API from '../API';
+// Component:
+import HeroImage from './HeroImage/HeroImage';
 // Image:
 import NoImage from '../assets/no_image.jpg';
 
 const Home = () => {
   // Take pieces of state from custom hook:
   const { state, isLoading, error } = useHomeFetchMovies();
-  console.log(state);
 
   return (
-    <div>Home</div>
+    <>
+      { state.results[0]
+        && <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            text={`${state.results[0].overview}`}
+            title={`${state.results[0].original_title}`}
+          />
+      }
+    </>
   );
 };
 
