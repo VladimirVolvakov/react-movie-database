ecf3f107037db02b6a7d61a4887665173991c348
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-07 00:05:57 +0300

    feat: add LoadMoreButton component styles

--- src/components/LoadMoreButton/LoadMoreButton.styles.js
+++ src/components/LoadMoreButton/LoadMoreButton.styles.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.button``;
+export const Wrapper = styled.button`
+  display: block;
+  background: var(--darkGrey);
+  height: 60px;
+  width: 25%;
+  min-width: 200px;
+  border: 0;
+  border-radius: 25px;
+  margin: 20px auto;
+  color: var(--white);
+  font-size: var(--fontLarge);
+  transition: all 0.3s;
+  outline: none;
+  cursor: pointer;
+  
+  :hover {
+    opacity: 0.8;
+  }
+`;
