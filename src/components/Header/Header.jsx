2bc3ce9220b0e56838f5168384c824467386e201
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-07 13:39:57 +0300

    feat: add routing for logos and  movie thumbnails

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,19 @@ import RMDBLogo from '../../assets/react-movie-logo.svg';
 import TMDBLogo from '../../assets/tmdb_logo.svg';
 // Styles:
 import { Wrapper, Content, Logo, TheMovieDatabaseLogo } from './Header.styles';
+// Routing:
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Wrapper>
       <Content>
-        <Logo src={RMDBLogo} alt='React movie database logo' />
-        <TheMovieDatabaseLogo src={TMDBLogo} alt='TheMovieDataBase logo' />
+        <Link to='/'>
+          <Logo src={RMDBLogo} alt='React movie database logo' />
+        </Link>
+        <Link to='https://www.themoviedb.org/'>
+          <TheMovieDatabaseLogo src={TMDBLogo} alt='TheMovieDataBase logo' />
+        </Link>
       </Content>
     </Wrapper>
   );
--- src/components/Thumbnail/Thumbnail.jsx
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -1,10 +1,21 @@
 // Styles:
-import { Image } from "./Thumbnail.styles";
+import { Image } from './Thumbnail.styles';
+// Routing:
+import { Link } from 'react-router-dom';
 
 const Thumbnail = ({ imageSrc, movieId, isClickable }) => {
   return (
     <div>
-      <Image src={imageSrc} alt="movie-thumbnail" />
+      { isClickable
+        ? (
+          <Link to={`/${movieId}`}>
+            <Image src={imageSrc} alt='movie-thumbnail' />
+          </Link>
+        )
+        : (
+          <Image src={imageSrc} alt='movie-thumbnail' />
+        )
+      }
     </div>
   );
 };
