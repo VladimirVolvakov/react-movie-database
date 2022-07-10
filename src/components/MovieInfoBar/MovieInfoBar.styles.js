2ea0d5188e93a95bda7d5fc4a3d15044aeeaf0e5
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-10 22:50:30 +0300

    feat: add MovieInfoBar component styles

--- src/components/MovieInfoBar/MovieInfoBar.styles.js
+++ src/components/MovieInfoBar/MovieInfoBar.styles.js
@@ -1,5 +1,42 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  min-height: 100px;
+  background: var(--darkGrey);
+  padding: 0 20px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+
+  .column {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: var(--mediumGrey);
+    margin: 0 20px;
+    border-radius: 20px;
+    flex: 1;
+
+    :first-child {
+      margin-left: 0;
+    }
+
+    :last-child {
+      margin-right: 0;
+    }
+  }
+
+  @media all and (max-width: 768px) {
+    display: block;
+
+    .column {
+      margin: 20px 0;
+    }
+  }
+`;
